46196effa0fbd174a98b1b41a9c086974111adbe
Enol Cachero Ramírez <enol@23xy.enol.com>
2021-05-18 14:10:03 +0200

    Se implementa contacto

--- src/Contacto.js
+++ src/Contacto.js
@@ -1,10 +1,48 @@
 import './App.css';
+import emailjs from 'emailjs-com';
 import React from 'react';
 
 function Contacto() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs.sendForm('service_2kkxkj1', 'template_51eqblc', e.target, 'user_AxO7ORYJihsgfH2jT5H07')
+      .then(
+        result => console.log(result.text),
+        err => console.log(err.text)
+      );
+
+    e.target.reset();
+  }
+
   return (
     <div className="Contacto">
-        <h1>This is the Contacto</h1>
+      {/* <form className="contacto-form" onSubmit={sendEmail}>
+          <input type="text" name="user_name" placeholder="Nombre"/>
+          <input type="text" name="user_email" placeholder="Email"/>
+          <input type="text" name="user_message" placeholder="Mensaje"/>
+          <input type="submit" value="Send"/>
+        </form> */}
+      <form onSubmit={sendEmail}> 
+        <div className="form-row">
+          <div className="col-6">
+            <input type="text" className="form-control" name="user_name" placeholder="Nombre" />
+          </div>
+          <div className="col-6">
+            <input type="text" className="form-control" name="user_email" placeholder="Correo" />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="col-12">
+            <input type="text" className="form-control" name="user_message" placeholder="Mensaje" />
+          </div>
+        </div>
+        <div className="form-row">
+        <div className="col-2">
+          <input type="submit" value="Send" />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
