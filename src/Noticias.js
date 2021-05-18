eee88198e1894bfaaba74508a27322c9b83f908d
Enol Cachero Ramírez <enol@23xy.enol.com>
2021-05-18 13:21:49 +0200

    Implementacion news bucle

--- src/Noticias.js
+++ src/Noticias.js
@@ -1,10 +1,39 @@
 import './App.css';
-import React from 'react';
+import React , {useState, useEffect} from 'react';
 
 function Noticias() {
+
+  useEffect( () => {
+    fetchItems();
+  }, []);
+
+  const [ items, setItems ] = useState([]);
+
+  const fetchItems = async() => {
+    const data = await fetch('http://localhost:3032/api/news', {
+      method: 'GET',
+      mode: 'cors'
+    });
+    const items = await data.json();
+    console.log(items.result);
+    setItems(items.result);
+  }
+
   return (
-    <div className="Noticias">
-        <h1>This is the Noticias</h1>
+    <div className="noticias-container">
+      {items.map( item => {
+          return(
+          <div class="card">
+           <div class="card-header">{item.date}</div>
+           <div class="card-body">
+            <h5 class="card-title">{item.name}</h5>
+            <p class="card-text">{item.description}</p>
+            <a href={item.link} class="btn btn-primary">Ir al evento</a>
+          </div>
+        </div>
+        )
+      })}
+      
     </div>
   );
 }
