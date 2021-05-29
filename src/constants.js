2cea71bda21b1231eff8d742ca9d130811f170b3
Enol Cachero Ramírez <enol@23xy.enol.com>
2021-05-29 17:51:15 +0200

    Cambio puerto

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
-// export const LOCALHOST = 'localhost';
-export const LOCALHOST = '46.183.114.163';
+export const LOCALHOST = 'localhost';
+// export const LOCALHOST = '46.183.114.163';
 export const PORT = '8080';
 export const TAB_CARRUSEL = '1';
 export const TAB_PINTURAS = '2';
 export const TAB_BIO = '3';
 export const TAB_CONTACTO = '4';
+
+export const TEXTO_LEGAL ="";
